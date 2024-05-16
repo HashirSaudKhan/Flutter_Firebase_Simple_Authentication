b8d4f9eb93463238bf78dda7ad4d570c03f8a6fd
Hashir Saud Khan <hashirsaudkhan@gmail.com>
2024-05-16 18:05:02 +0500

    flutter fetch data from firebase realtime database list

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,11 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_firebase_ui/screens/login_screen.dart';
 import 'package:flutter_firebase_ui/screens/posts/addposts.dart';
 import 'package:flutter_firebase_ui/utils/utils.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+// firebaseDatabase reference
+  final ref = FirebaseDatabase.instance.ref('Post');
 
 // user sign out
   Future<void> signUserOut() async {
@@ -23,8 +34,10 @@ class HomeScreen extends StatelessWidget {
           IconButton(
               onPressed: () {
                 signUserOut().then((value) {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()));
                 }).onError((error, stackTrace) {
                   debugPrint(error.toString());
                   Utils.toastmsg(context, error.toString());
@@ -38,6 +51,44 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
+            Expanded(
+                child: StreamBuilder(
+                    stream: ref.onValue,
+                    builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                      if (!snapshot.hasData) {
+                        return const CircularProgressIndicator();
+                      } else {
+                        // create map for firebaseDatabase data
+                        Map<dynamic, dynamic> map =
+                            snapshot.data!.snapshot.value as dynamic;
+
+                        //crate empty list
+                        List<dynamic> list = [];
+                        list.clear();
+
+                        //convert map values to list
+                        list = map.values.toList();
+
+                        return ListView.builder(
+                            itemCount: snapshot.data!.snapshot.children.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(list[index]['title']),
+                                subtitle: Text(list[index]['id']),
+                              );
+                            });
+                      }
+                    })),
+            Expanded(
+              child: FirebaseAnimatedList(
+                  query: ref,
+                  itemBuilder: (context, snapshot, Animation, index) {
+                    return ListTile(
+                      title: Text(snapshot.child('title').value.toString()),
+                      subtitle: Text(snapshot.child('id').value.toString()),
+                    );
+                  }),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
